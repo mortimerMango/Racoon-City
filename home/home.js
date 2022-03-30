84b029349abec5ee234d99860c4766342dc5b1a7
mortimerMango <94403406+mortimerMango@users.noreply.github.com>
2022-03-30 00:31:00 -0700

    commenting home js file

--- home/home.js
+++ home/home.js
@@ -4,8 +4,8 @@ function theDay(){
 	const week = ['sunday.gif', 'monday.gif', 'tuesday.gif', 'wednesday.gif', 'thursday.gif', 'friday.gif', 'saturday.gif'];
 	const day = new Date();
 	let dayOf = day.getDay();
-
-	var test = "url(\"file:///C:/Users/teran/Desktop/Orugasumu/ProjectAlpha/home/img/" + week[dayOf] + "\")";
+	//use local computer full path to render correct image for racoon day home/img/
+	var test = "url(\"file:///C:/put full path here to home/img/" + week[dayOf] + "\")";
 	document.getElementById(['op-day']).style.backgroundImage = test;
 }
 
